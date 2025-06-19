68c8198d8a508a11c0e17edfe4369aa8a692630f
Aryan Khandelwal <2002ak2002@gmail.com>
2025-06-19 10:33:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+/* script.js */
+function scrollToCollections() {
+  document.getElementById('collections').scrollIntoView({ behavior: 'smooth' });
+  console.log('Navigated to collections section');
+}
