7cb5748301923fd4056e3733967647ffd96a22a0
Aryan Khandelwal <2002ak2002@gmail.com>
2025-08-04 19:22:01 +0530

    LastCommit

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function scrollToCollections() {
   document.getElementById('collections').scrollIntoView({ behavior: 'smooth' });
   console.log('Navigated to collections section');
 }
+
+document.querySelectorAll('.product-image').forEach(function(imgDiv) {
+  const images = JSON.parse(imgDiv.getAttribute('data-images'));
+  let idx = 0;
+  const mainImg = imgDiv.querySelector('.main-img');
+  let interval;
+
+  imgDiv.addEventListener('mouseenter', function() {
+    interval = setInterval(function() {
+      idx = (idx + 1) % images.length;
+      mainImg.src = images[idx];
+    }, 1000); // Change image every 600ms
+  });
+
+  imgDiv.addEventListener('mouseleave', function() {
+    clearInterval(interval);
+    idx = 0;
+    mainImg.src = images[0]; // Reset to first image
+  });
+});
